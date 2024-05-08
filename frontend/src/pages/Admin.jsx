828e3b246e0f99d716cc23c750009a4fc8da94a0
Akos Oravecz <oraveczakos2001@gmail.com>
2024-05-08 14:39:46 +0200

    add users and invoice tables

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AxiosContext } from '../contexts/AxiosContext';
+import { Table } from 'react-bootstrap';
 
 export default function Admin() {
+  const { publicAxios } = useContext(AxiosContext);
+  const [ users, setUsers ] = useState([]);
+
+  const usersRequest = () => {
+    publicAxios.get("/auth/all_user").then(res => {
+      setUsers(res.data);
+    }).then(error => {
+      console.log(JSON.stringify(error));
+    })
+  }
+  
+  useEffect(() => {
+    usersRequest();
+  }, []);
+
   return (
-    <div>
-      Admin page
-    </div>
+    <>
+      <h1>Users</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Roles</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            users.map(user =>
+              <tr key={user["username"]}>
+                <td>{user["name"]}</td>
+                <td>{user["username"]}</td>
+                <td>{user["roles"].map(role => role.name).join(", ")}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
+    </>
   )
 }
--- frontend/src/pages/Invoices.jsx
+++ frontend/src/pages/Invoices.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { AxiosContext } from '../contexts/AxiosContext'
+import { Table } from 'react-bootstrap';
 
 export default function Invoices() {
+  const { authAxios } = useContext(AxiosContext);
+  const [ invoices, setInvoices ] = useState([]);
+
+  const invoiceRequest = () => {
+    authAxios.get("/invoice/").then(res => {
+      setInvoices(res.data);
+    }).then(error => {
+      console.log(JSON.stringify(error));
+    })
+  }
+  
+  useEffect(() => {
+    invoiceRequest();
+  }, []);
   return (
-    <div>
-      Invoice
-    </div>
+    <>
+      <h1>Invoices</h1>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Customer name</th>
+            <th>Invoice date</th>
+            <th>Due date</th>
+            <th>Itemname</th>
+            <th>Description</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            invoices.map((invoice, i) =>
+              <tr key={i}>
+                <td>{invoice["customerName"]}</td>
+                <td>{invoice["invoiceDate"]}</td>
+                <td>{invoice["dueDate"]}</td>
+                <td>{invoice["itemName"]}</td>
+                <td>{invoice["description"]}</td>
+                <td>{invoice["price"]}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
+    </>
   )
 }
